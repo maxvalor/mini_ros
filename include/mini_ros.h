9f382dcb16e1b17240362bde1469c90c238707b9
mayong <mayong@indemind.cn>
2020-06-22 16:46:28 +0800

    implement Publisher and Subscriber

--- include/mini_ros.h
+++ include/mini_ros.h
@@ -8,11 +8,30 @@
 #include <memory>
 #include <cstring>
 #include <future>
+#include <typeinfo>
 #include "message.h"
 
 #include <iostream>
 
 namespace mini_ros {
+  //
+  // class Subscriber {
+  //   std::string topic;
+  //   const ;
+  //
+  // public:
+  //   Subscriber() {}
+  //   ~Subscriber() {}
+  //   Subscriber(std::string topic, const std::type_info &nInfo) : topic(topic), nInfo(nInfo) {}
+  //   bool shutdown() {
+  //     ModuleHandler& mh = ModuleHandler::instance();
+  //     mh.remove(topic, nInfo);
+  //   }
+  // };
+  template <typename T>
+  class Subscriber;
+  template <typename T>
+  class Publisher;
 
   class ModuleHandler {
     using func_vector = std::vector<std::function<void(std::shared_ptr<Message>)>>;
@@ -37,23 +56,33 @@ namespace mini_ros {
     }
 
     template <typename T>
-    void subscribe(std::string topic, std::function<void(std::shared_ptr<T> msg)> f) {
+    Subscriber<T> subscribe(std::string topic, std::function<void(std::shared_ptr<T> msg)> f) {
       auto packed_f = [f](std::shared_ptr<Message> msg){
         f(std::static_pointer_cast<T>(msg));
       };
+      std::cout << typeid(packed_f).name() << std::endl;
+      size_t index;
       try {
         auto& funcs = topic_callbacks.at(topic);
         //std::cout << "func size:" << funcs.size() << std::endl;
         funcs.push_back(packed_f);
+        index = funcs.size() - 1;
         // topic_callbacks.insert(
         //   std::pair<std::string, func_vector>(topic, funcs));
       }
       catch (std::out_of_range e) {
         func_vector funcs;
         funcs.push_back(packed_f);
+        index = funcs.size() - 1;
         topic_callbacks.insert(
           std::pair<std::string, func_vector>(topic, funcs));
       }
+
+      auto shutdown_f = [this, index, topic] {
+        remove(topic, index);
+      };
+
+      return Subscriber<T>(shutdown_f);
     }
 
     template <typename T>
@@ -80,7 +109,49 @@ namespace mini_ros {
       }
     }
 
-};
+    template <typename T>
+    Publisher<T> advertise(std::string topic) {
+      return Publisher<T>(topic);
+    }
+
+    bool remove(std::string topic, std::uint32_t index) {
+      try {
+        auto &funcs = topic_callbacks.at(topic);
+        funcs.erase(funcs.begin() + index);
+      }
+      catch (std::out_of_range e) {
+        // do nothing.
+        std::cout << "error" << std::endl;
+      }
+    }
+
+  };
+
+  template <typename T>
+  class Publisher {
+    //ModuleHandler &mh;
+    std::string topic;
+
+  public:
+    Publisher(std::string topic) : topic(topic) {
+      //mh = std::ref(mini_ros::ModuleHandler::instance());
+    }
+    void publish(T& msg) {
+      ModuleHandler::instance().publish<T>(topic, msg);
+    }
+  };
+
+  template <typename T>
+  class Subscriber {
+    std::function<void(void)> f;
+  public:
+    Subscriber() {}
+    Subscriber(std::function<void(void)> f) : f(f) {}
+
+    void shutdown() {
+      f();
+    }
+  };
 
   ModuleHandler* ModuleHandler::singleton = nullptr;
 }
--- sample/main.cpp
+++ sample/main.cpp
@@ -3,6 +3,9 @@
 #include <thread>
 #include <iostream>
 
+static int c;
+mini_ros::Subscriber<TestMsg> sub;
+
 void on_msg(std::shared_ptr<TestMsg> msg) {
   std::cout << "1:received " << msg->data << std::endl;
 }
@@ -13,20 +16,25 @@ void on_msg2(std::shared_ptr<TestMsg> msg) {
 
 void on_msg3(std::shared_ptr<TestMsg> msg) {
   std::cout << "3:received " << msg->data << std::endl;
+  if (msg->data == 5) {
+    sub.shutdown();
+  }
 }
 
 int main() {
   mini_ros::ModuleHandler& mh = mini_ros::ModuleHandler::instance();
   mh.subscribe<TestMsg>("test", on_msg);
   mh.subscribe<TestMsg>("test", on_msg2);
-  mh.subscribe<TestMsg>("test", on_msg3);
+  sub = mh.subscribe<TestMsg>("test", on_msg3);
   std::thread t([&mh](){
-    int i = 200;
+    int i = 10;
+    mini_ros::Publisher<TestMsg> pub = mh.advertise<TestMsg>("test");
     while (i--) {
       TestMsg msg;
       msg.data = i;
       std::cout << "publish " << msg.data << std::endl;
-      mh.publish<TestMsg>("test", msg);
+      //mh.publish<TestMsg>("test", msg);
+      pub.publish(msg);
     }
   });
   t.join();
